18b238c89f8f3a2b9168f6fb6ebea903a4f9ea57
Abu Shahrukh <abushahrukh6@gmail.com>
2024-10-21 21:04:48 +0500

    Create add_sub_mul_div_rem.rs

---
+++ add_sub_mul_div_rem.rs
@@ -0,0 +1,13 @@
+fn main(){
+  //addition
+let sum = 5 +10;
+// Subtraction
+let diffrence = 95.5 - 4.3;
+// multipication
+let product = 4*30;
+// division
+let quotient = 56.7/32;
+let truncated = -5/3; // results in -1
+// remainder
+let remainder = 43%5;
+}
